a7acd0eb7803a7f48f8e9f81c9de82b550fafbf7
Kevin <kevinyluo@gmail.com>
2019-11-07 17:02:34 -0800

    Testing Changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import Visualizer from './Visualizer/Visualizer';
 import './App.css';
+//import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
